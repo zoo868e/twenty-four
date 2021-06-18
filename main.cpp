9680ad055b4b97e09ce8ce1c555f33acb47c4808
zoo868e <zoo868e@gmail.com>
2021-06-19 01:12:58 +0800

    Fix the Bug
    When input the 5 5 7 11 to old version, it can't caluclate the answer.
    And the output are float, but the game is use fraction number.
    So I change the used data structure. To make the output correct.
    Add class num{
    	int up = Denomiator, down = molecular;
    }

--- main.cpp
+++ main.cpp
@@ -10,45 +10,94 @@ using namespace std;
 char syntax[7] = "+-*/()";
 string answer = "";
 
-bool gotit(vector<float> a){
-	return (a.size() == 1 && a[0] == 24? true:false);
+class num{
+	public:
+		int up;
+		int down;
+		num(int up, int down):up{up},down{down}{}
+		num(int val):num(val, 1){}
+		~num()=default;
+		friend ostream& operator<<(ostream& o, const num& x){
+			if(x.down != 1)
+				o << "(" << x.up << "/" << x.down << ")";
+			else
+				o << x.up ;
+			return o;
+		}
+};
+bool gotit(vector<num> a){
+	if(a[0].up % a[0].down)return false;
+	else if(a.size() > 1)return false;
+	else if(a[0].up / a[0].down != 24)return false;
+	else return true;
 }
 
-bool dfs(vector<float> a){
-	if(gotit(a)){
-		cout << a[0] << endl;
-		return true;
+
+bool dfs(vector<num> a){
+/*	cout << a.size() << endl;
+	cout << "this turn a : ";
+	for(int i = 0;i < (int)a.size();i++){
+		cout << a[i] << " ";
+	}
+	cout << endl;
+*/
+	for(int i = 0;i < (int)a.size();i++){
+		if(a[i].up % a[i].down);
+		else {
+			a[i].up = a[i].up / a[i].down;
+			a[i].down = 1;
+		}
+	}
+	if(a.size() == 1){
+		if(gotit(a)){
+//			cout << a[0] << endl;
+			return true;
+		}
+		else return false;
 	}
 	else if(a.size() > 1){
-		for(int i = 0;i < (int)a.size() - 1;i++){
-			for(int j = i + 1;j < (int)a.size();j++){
-				vector<float> b;
-				float x = a[i], y = a[j];
+		for(int i = 0;i < (int)a.size();i++){
+			for(int j = 0;j < (int)a.size();j++){
+				if(j == i)continue;
+				vector<num> b;
+				num x = a[i], y = a[j];
 				for(int k = 0;k < (int)a.size();k++){
 					if(k == i || k == j);
 					else b.push_back(a[k]);
 				}
-				b.push_back(x + y);
+				bool flag = false;
+				if(x.up * y.down + x.down * y.up != 0){
+					b.push_back(num(x.up * y.down + x.down * y.up, x.down * y.down));
+					flag = true;
+				}
 				if(dfs(b)){
-					cout << x << "+" << y << endl;
+					cout  << "do this! " << x << "+" << y << endl;
+					return true;
+				}
+				if(flag){
+					b.pop_back();
+				}
+				flag = false;
+				if(x.up * y.down - x.down * y.up != 0){
+					b.push_back(num(x.up * y.down - x.down * y.up, x.down * y.down));
+					flag = true;
+				}
+				if(dfs(b)){
+					cout << "do this! " << x << "-" << y << endl;
+					return true;
+				}
+				if(flag){
+					b.pop_back();
+				}
+				b.push_back(num(x.up * y.up, x.down * y.down));
+				if(dfs(b)){
+					cout  << "do this! " << x << "*" << y << endl;
 					return true;
 				}
 				b.pop_back();
-				b.push_back(x - y);
+				b.push_back(num(x.up * y.down, x.down * y.up));
 				if(dfs(b)){
-					cout << x << "-" << y << endl;
-					return true;
-				}
-				b.pop_back();
-				b.push_back(x * y);
-				if(dfs(b)){
-					cout << x << "*" << y << endl;
-					return true;
-				}
-				b.pop_back();
-				b.push_back(x / y);
-				if(dfs(b)){
-					cout << x << "/" << y << endl;
+					cout  << "do this! " << x << "/" << y << endl;
 					return true;
 				}
 			}
@@ -59,10 +108,13 @@ bool dfs(vector<float> a){
 }
 
 void solve(){
-	vector<float> a;
-	a.resize(4);
-	cin >> a[0] >> a[1] >> a[2] >> a[3];
-	dfs(a);
+	vector<num> a;
+	int t;
+	for(int i = 0;i < 4;i++){
+		cin >> t;
+		a.push_back(num(t));
+	}
+	if(!dfs(a))cout << "I can't calculate an answer.\n";
 }
 
 int main(){
